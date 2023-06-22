777525ac0d41f982edfbbfeca02efb72d2ab7603
Rivière Mathis <mathis.riviere@edu.devinci.fr>
2023-06-22 11:24:19 +0200

    added real text and glassmorphism to planets

--- Cours/src/script.js
+++ Cours/src/script.js
@@ -205,6 +205,8 @@ function onMouseMove(event) {
 canvas.addEventListener('mousedown', onMouseDown, false);
 canvas.addEventListener('mousemove', onMouseMove, false);
 
+
+
 function onClick(event) {
   const rect = canvas.getBoundingClientRect();
   const x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
@@ -223,104 +225,109 @@ function onClick(event) {
   // Get the text container element
   const textContainer = document.getElementById('text-container');
 
+  textContainer.style.display = 'none';
+
   // Clear previous text content
   textContainer.innerHTML = '';
 
   const planetText = {
     mercure: {
-      title: "Mercury",
-      paragraphs: [
-        { text: "Paragraph 1 for Mercury...", class: "paragraph1" },
-        { text: "Paragraph 2 for Mercury...", class: "paragraph2" },
-        { text: "Paragraph 3 for Mercury...", class: "paragraph3" }
-      ]
+      title: "Mercure",
+      size: "2 439 km",
+      distance: " 57,910,000 km",
+      description: "Mercure est étonnamment dense, et cela est apparemment dû au fait qu’elle ait un noyau ferreux particulièrement important. L’atmosphère ténue de Mercure laisse à sa surface le témoignage de bombardements d’astéroïdes ayant eu lieu dans sa jeunesse."
     },
     venus: {
       title: "Venus",
-      paragraphs: [
-        { text: "Paragraph 1 for Venus...", class: "paragraph1" },
-        { text: "Paragraph 2 for Venus...", class: "paragraph2" },
-        { text: "Paragraph 3 for Venus...", class: "paragraph3" }
-      ]
+      size: "6 052 km",
+      distance: "108,200,000 km",
+      description: "Vénus possède une atmosphère de dioxyde de carbone 90 fois plus épaisse que sur la Terre, ce qui provoque un remarquable effet de serre. Il en résulte une température extrême, la plus brûlante de toutes les planètes : il y règne une température d’environ 477°C !"
     },
     terre: {
       title: "Earth",
-      paragraphs: [
-        { text: "Paragraph 1 for Earth...", class: "paragraph1" },
-        { text: "Paragraph 2 for Earth...", class: "paragraph2" },
-        { text: "Paragraph 3 for Earth...", class: "paragraph3" }
-      ]
+      size: "6 378 km",
+      distance: "149,600,000 km",
+      description: "La Terre est à ce jour la seule planète connue pour abriter en abondance de l’eau à l’état liquide et de la vie. C’est également la planète la plus connue de l’homme. Depuis des siècles elle est étudiée dans ses coins les plus reculés."
     },
     mars: {
       title: "Mars",
-      paragraphs: [
-        { text: "Paragraph 1 for Mars...", class: "paragraph1" },
-        { text: "Paragraph 2 for Mars...", class: "paragraph2" },
-        { text: "Paragraph 3 for Mars...", class: "paragraph3" }
-      ]
+      size: "3 397 km",
+      distance: "227,940,000 km",
+      description: "En 2004, les astronomes dirigeant la mission « Mars Exploration Rover » de la NASA ont confirmé que de l’eau à l’état liquide exista jadis sur la surface de Mars ! Auparavant, les scientifiques avaient émis l’hypothèse de l’existence d’eau sur Mars en des temps reculés, en raison de l’état de sa surface qui ressemblait à de l’érosion, comme ce que l’on trouve sur Terre. Aujourd’hui, l’atmosphère de dioxyde de carbone présente sur Mars est tellement fine que c’est une planète froide et sèche, avec des pôles d’eau gelée et de dioxyde de carbone. Mais il semble que de petits jets d’eau provenant de la croûte martienne continuent de remonter à la surface par endroits."
     },
     jupiter: {
       title: "Jupiter",
-      paragraphs: [
-        { text: "Paragraph 1 for Jupiter...", class: "paragraph1" },
-        { text: "Paragraph 2 for Jupiter...", class: "paragraph2" },
-        { text: "Paragraph 3 for Jupiter...", class: "paragraph3" }
-      ]
+      size: "71 490 km",
+      distance: "778,330,000 km",
+      description: "Jupiter est la plus grosse planète de notre Système solaire. Son atmosphère d’hélium et d’hydrogène contient de gros nuages colorés, et son immense magnétosphère, ses anneaux et satellites en font à elle seule un véritable système planétaire. Une des plus grosses lunes de Jupiter, Io, possède des volcans qui font de sa surface la plus chaude de tout le système solaire. Au moins quatre des satellites de Jupiter possèdent une atmosphère, et au moins trois sont susceptibles de contenir de l’eau à l’état liquide ou partiellement gelée."
     },
     saturne: {
-      title: "Saturne",
-      paragraphs: [
-        { text: "Paragraph 1 for Saturne...", class: "paragraph1" },
-        { text: "Paragraph 2 for Saturne...", class: "paragraph2" },
-        { text: "Paragraph 3 for Saturne...", class: "paragraph3" }
-      ]
+      title: "Saturn",
+      size: "60 268 km",
+      distance: "1,429,400,000 km",
+      description: "Saturne, la rivale de Jupiter, possède un système d’anneaux bien plus complexe et un nombre équivalent de satellites. Titan, l’un des satellites de Saturne, possède l’atmosphère la plus épaisse de tous les autres satellites du système solaire. Une autre lune de Saturne, Encelade, possède des geysers d’eau liquide. Quant à Uranus et Neptune, elles possèdent peu d’hydrogène en proportion de Jupiter et Saturne ; Uranus, qui possède également des anneaux, a la particularité d’avoir une rotation à 98° sur le plan de son orbite."
     },
     uranus: {
       title: "Uranus",
-      paragraphs: [
-        { text: "Paragraph 1 for Uranus...", class: "paragraph1" },
-        { text: "Paragraph 2 for Uranus...", class: "paragraph2" },
-        { text: "Paragraph 3 for Uranus...", class: "paragraph3" }
-      ]
+      size: "25 559 km",
+      distance: "2,870,990,000 km",
+      description: "Septième planète du système solaire, Uranus est une géante gazeuse bleue. On ne connait pas très bien la composition interne de cette planète, mais on est certain qu’elle est différente de celle de Jupiter et Saturne. Sa couleur bleue est due à une forte présence de méthane."
     },
     neptune: {
       title: "Neptune",
-      paragraphs: [
-        { text: "Paragraph 1 for Neptune...", class: "paragraph1" },
-        { text: "Paragraph 2 for Neptune...", class: "paragraph2" },
-        { text: "Paragraph 3 for Neptune...", class: "paragraph3" }
-      ]
+      size: "25 269 km",
+      distance: "4,498,250,000 km",
+      description: "Elle est la dernière géante gazeuse de notre système solaire. Il n’est possible de l’observer qu’à l’aide d’un télescope. Les scientifiques pensent que sa composition interne est similaire à celle d’Uranus, de même que sa couleur bleue est due à la présence de méthane."
     }
   };
 
   // Declare a flag to track the zoom state
   if (intersects.length > 0) {
     const object = intersects[0].object;
+    const planetName = object.name.toLowerCase();
     console.log(`Planet clicked: ${object.name}`);
 
+    textContainer.style.display = 'block';
 
-    // Display planet information
-    const planetInfo = document.createElement('div');
-    planetInfo.innerHTML = `<h2>${object.name}</h2>`;
 
-    const paragraphs = planetText[object.name.toLowerCase()].paragraphs;
-    paragraphs.forEach((paragraphData) => {
-      const planetParagraph = document.createElement('p');
-      planetParagraph.textContent = paragraphData.text;
-      planetParagraph.classList.add(paragraphData.class);
-      planetInfo.appendChild(planetParagraph);
-    });
+    if (planetText.hasOwnProperty(planetName)) {
+      // Clear previous text content
+      textContainer.innerHTML = '';
 
-    // Add the fade-in class to the text container
-    textContainer.classList.add('fade-in');
+      // Display planet information
+      const planetInfo = document.createElement('div');
+      planetInfo.innerHTML = `
+        <h2 class="planet-title">${planetText[planetName].title}</h2>
+        <p class="planet-size"><strong>Taille:</strong> ${planetText[planetName].size}</p>
+        <p class="planet-distance"><strong>Distance du Soleil:</strong> ${planetText[planetName].distance}</p>
+        <p class="planet-description">${planetText[planetName].description}</p>
+      `;
 
-    // Add the planet information to the text container
-    textContainer.appendChild(planetInfo);
+      // Remove existing planet-specific class from the text container
+      const planetClasses = ['mercure', 'venus', 'terre', 'mars', 'jupiter', 'saturne', 'uranus', 'neptune'];
+      textContainer.classList.remove(...planetClasses);
+      // Add planet-specific class to the text container
+      textContainer.classList.add(planetName);
 
-    // Trigger the fade-in effect by adding the show class after a short delay
-    setTimeout(() => {
-      textContainer.classList.add('show');
-    }, 100);
+      // Add planet-specific class to the text container
+      const planetClassName = object.name.toLowerCase();
+      // textContainer.className = 'text-container';
+      textContainer.classList.add('text-container', planetClassName);
+
+      // Update the class of the text container
+      textContainer.className = `text-container ${planetName}`;
+
+      // Add the fade-in class to the text container
+      textContainer.classList.add('fade-in');
+
+      // Add the planet information to the text container
+      textContainer.appendChild(planetInfo);
+
+      // Trigger the fade-in effect by adding the show class after a short delay
+      setTimeout(() => {
+        textContainer.classList.add('show');
+      }, 100);
+    }
 
     // Zoom
     const targetPosition = object.position.clone();
@@ -347,6 +354,8 @@ function onClick(event) {
     const zoomOutPosition = new THREE.Vector3(0, 0, 200); // Adjust the unzoomed camera position
     const tweenDuration = 1000;
 
+    textContainer.style.display = 'none';
+
     new TWEEN.Tween(camera.position)
       .to(zoomOutPosition, tweenDuration)
       .easing(TWEEN.Easing.Quadratic.InOut)
@@ -363,7 +372,7 @@ function onClick(event) {
       .easing(TWEEN.Easing.Quadratic.InOut)
       .start();
 
-      // Add the fade-out class to the text container
+    // Add the fade-out class to the text container
     textContainer.classList.add('fade-out');
 
     // Remove the show class to trigger the fade-out effect
@@ -376,7 +385,6 @@ function onClick(event) {
   }
 
 }
-
 
 
 animate()
